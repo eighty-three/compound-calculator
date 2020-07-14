22a13b70e8b9cd7905f0d50f17e658a1a33ee7e2
Eighty Three <cwp1@protonmail.com>
2020-07-15 05:36:35 +0800

    Remove unused file

--- client/src/lib/convert.js
+++
@@ -1,7 +0,0 @@
-export const findTotal = ({ principal, years, monthly, rate }) => {
-  const totalAmount = 
-    (principal * (1 + rate/100)**years) +
-    (monthly * 12 * (((1 + rate/100)**years - 1) / (rate/100)));
-  return Math.trunc(totalAmount); 
-};
-
