49cae7263f0af8c130ee2d22369789c403c5eaa5
Eighty Three <cwp1@protonmail.com>
2020-07-20 08:34:18 +0800

    Remove unnecessary SWR conditional in findTotal function

--- client/src/lib/calculate.js
+++ client/src/lib/calculate.js
@@ -2,14 +2,12 @@ export const twoDecimals = (number) => Math.round((number + Number.EPSILON) * 10
 
 export const getSWR = (amount) => twoDecimals(amount * .04 / 12); //Always 4%
 
-export const findTotal = ({ principal = 0, years = 0, monthly = 0, rate = 0, swr }) => {
+export const findTotal = ({ principal = 0, years = 0, monthly = 0, rate = 0 }) => {
   const forPrincipal = principal * (1 + rate/100) ** years;
   const totalAmount = (rate > 0) 
     ? forPrincipal + (monthly * 12 * (((1 + rate/100)**years - 1) / (rate/100)))
     : forPrincipal + monthly * 12 * years;
-  return (swr)
-    ? getSWR(totalAmount)
-    : twoDecimals(totalAmount);
+  return twoDecimals(totalAmount);
 };
 
 export const findYears = ({ goal = 0, monthly = 0, rate = 0, swr }) => {
